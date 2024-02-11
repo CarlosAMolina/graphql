7c5b8a74d381670674e8b765e787f3d2b98123b3
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-02-11 14:06:40 +0100

    Change import

--- flask_sqlalchemy/src/models.py
+++ flask_sqlalchemy/src/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import *
+import sqlalchemy as sa
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("sqlite:///database.sqlite3")
+engine = sa.create_engine("sqlite:///database.sqlite3")
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
@@ -12,14 +12,14 @@ Base.query = db_session.query_property()
 
 class Department(Base):
     __tablename__ = "department"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
 
 
 class Employee(Base):
     __tablename__ = "employee"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    hired_on = Column(DateTime, default=func.now())
-    department_id = Column(Integer, ForeignKey("department.id"))
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+    hired_on = sa.Column(sa.DateTime, default=sa.func.now())
+    department_id = sa.Column(sa.Integer, sa.ForeignKey("department.id"))
     department = relationship(Department, backref=backref("employees", uselist=True, cascade="delete,all"))
