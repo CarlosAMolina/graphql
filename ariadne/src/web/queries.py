fae1185e475083a19026ba10d6bafd25cace0e07
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-28 23:06:06 +0100

    Add min aggregation. Refactor rename

--- ariadne/src/web/queries.py
+++ ariadne/src/web/queries.py
@@ -31,12 +31,16 @@ def resolve_aggregate_people(*_, function, field: str) -> tp.Optional[int]:
     https://dgraph.io/docs/graphql/queries/aggregate/
     https://developer.salesforce.com/docs/platform/graphql/guide/aggregate-examples.html
     """
-    if function == "max":
-        ages = [person[field] for person in data.people if person.get(field) is not None]
-        _assert_int_values(ages)
-        return None if len(ages) == 0 else max(ages)
+    field_values = [person[field] for person in data.people if person.get(field) is not None]
+    _assert_all_values_are_int(field_values)
+    if len(field_values) == 0:
+        return None
+    elif function == "max":
+        return max(field_values)
+    elif function == "min":
+        return min(field_values)
 
 
-def _assert_int_values(array):
+def _assert_all_values_are_int(array):
     if not all(isinstance(value, numbers.Number) for value in array):
         raise TypeError
