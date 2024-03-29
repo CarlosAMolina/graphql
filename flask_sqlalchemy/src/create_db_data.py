55e8ceb0e59f6e4a1952e743f2932a0a003bad2a
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-29 07:03:46 +0100

    Refactor extract variable

--- flask_sqlalchemy/src/create_db_data.py
+++ flask_sqlalchemy/src/create_db_data.py
@@ -3,7 +3,6 @@ from models import engine, db_session, Base, Department, Employee
 
 def run():
     Base.metadata.create_all(bind=engine)
-
     departments = {
         "engineering": Department(name="Engineering"),
         "hr": Department(name="Human Resources"),
@@ -13,10 +12,9 @@ def run():
         Employee(name="Roy", department=departments["engineering"]),
         Employee(name="Tracy", department=departments["hr"]),
     ]
-    for department in departments.values():
-        db_session.add(department)
-    for employee in employees:
-        db_session.add(employee)
+    rows_to_insert = list(departments.values()) + employees
+    for row in rows_to_insert:
+        db_session.add(row)
     db_session.commit()
 
 
