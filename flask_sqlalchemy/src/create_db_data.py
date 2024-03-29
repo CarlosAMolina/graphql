2e1c2ca6b3d1a1085e789272c449724dcddd0a6d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-29 06:59:21 +0100

    Refactor extract variable

--- flask_sqlalchemy/src/create_db_data.py
+++ flask_sqlalchemy/src/create_db_data.py
@@ -8,16 +8,15 @@ def run():
         "engineering": Department(name="Engineering"),
         "hr": Department(name="Human Resources"),
     }
-
-    peter = Employee(name="Peter", department=departments["engineering"])
-    roy = Employee(name="Roy", department=departments["engineering"])
-    tracy = Employee(name="Tracy", department=departments["hr"])
-
+    employees = {
+        "peter": Employee(name="Peter", department=departments["engineering"]),
+        "roy": Employee(name="Roy", department=departments["engineering"]),
+        "tracy": Employee(name="Tracy", department=departments["hr"]),
+    }
     for department in departments.values():
         db_session.add(department)
-    db_session.add(peter)
-    db_session.add(roy)
-    db_session.add(tracy)
+    for employee in employees.values():
+        db_session.add(employee)
     db_session.commit()
 
 
