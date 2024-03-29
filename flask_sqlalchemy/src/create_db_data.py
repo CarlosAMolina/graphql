af1792039450f9291df9b6648b2325ceb3f2ba24
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-29 07:06:51 +0100

    Refactor change import

--- flask_sqlalchemy/src/create_db_data.py
+++ flask_sqlalchemy/src/create_db_data.py
@@ -1,21 +1,21 @@
-from models import engine, db_session, Base, Department, Employee
+import models
 
 
 def run():
-    Base.metadata.create_all(bind=engine)
+    models.Base.metadata.create_all(bind=models.engine)
     departments = {
-        "engineering": Department(name="Engineering"),
-        "hr": Department(name="Human Resources"),
+        "engineering": models.Department(name="Engineering"),
+        "hr": models.Department(name="Human Resources"),
     }
     employees = [
-        Employee(name="Peter", department=departments["engineering"]),
-        Employee(name="Roy", department=departments["engineering"]),
-        Employee(name="Tracy", department=departments["hr"]),
+        models.Employee(name="Peter", department=departments["engineering"]),
+        models.Employee(name="Roy", department=departments["engineering"]),
+        models.Employee(name="Tracy", department=departments["hr"]),
     ]
     rows_to_insert = list(departments.values()) + employees
     for row in rows_to_insert:
-        db_session.add(row)
-    db_session.commit()
+        models.db_session.add(row)
+    models.db_session.commit()
 
 
 if __name__ == "__main__":
