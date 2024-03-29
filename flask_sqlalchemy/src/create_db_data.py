5773e85f7b186a00fc165c25c5227c8859bbce0b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-29 06:52:36 +0100

    Refactor extract function

--- flask_sqlalchemy/src/create_db_data.py
+++ flask_sqlalchemy/src/create_db_data.py
@@ -1,17 +1,23 @@
 from models import engine, db_session, Base, Department, Employee
 
-Base.metadata.create_all(bind=engine)
 
-engineering = Department(name="Engineering")
-hr = Department(name="Human Resources")
+def run():
+    Base.metadata.create_all(bind=engine)
 
-peter = Employee(name="Peter", department=engineering)
-roy = Employee(name="Roy", department=engineering)
-tracy = Employee(name="Tracy", department=hr)
+    engineering = Department(name="Engineering")
+    hr = Department(name="Human Resources")
 
-db_session.add(engineering)
-db_session.add(hr)
-db_session.add(peter)
-db_session.add(roy)
-db_session.add(tracy)
-db_session.commit()
+    peter = Employee(name="Peter", department=engineering)
+    roy = Employee(name="Roy", department=engineering)
+    tracy = Employee(name="Tracy", department=hr)
+
+    db_session.add(engineering)
+    db_session.add(hr)
+    db_session.add(peter)
+    db_session.add(roy)
+    db_session.add(tracy)
+    db_session.commit()
+
+
+if __name__ == "__main__":
+    run()
