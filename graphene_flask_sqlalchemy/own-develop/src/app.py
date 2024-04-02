5e83c5f8ca118febd953e6500acc89da5c8e28f3
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-04-02 23:59:55 +0200

    Add show GraphiQL URL

--- graphene_flask_sqlalchemy/own-develop/src/app.py
+++ graphene_flask_sqlalchemy/own-develop/src/app.py
@@ -7,8 +7,10 @@ import database
 app = Flask(__name__)
 app.debug = True
 
+URL_PATH_GRAPHQL = "graphql"
+
 app.add_url_rule(
-    "/graphql",
+    f"/{URL_PATH_GRAPHQL}",
     view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True),  # for having the GraphiQL interface
 )
 
@@ -21,4 +23,5 @@ def shutdown_session(exception=None):
 if __name__ == "__main__":
     database.init_db()
     print("Start run server")
+    print(f"GraphiQL URL: http://127.0.0.1:5000/{URL_PATH_GRAPHQL}")
     app.run()
