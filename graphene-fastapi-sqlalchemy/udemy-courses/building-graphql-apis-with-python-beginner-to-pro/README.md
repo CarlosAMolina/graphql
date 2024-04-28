3eaf3e04422d00b87ae9bbec1a358d0aeba29d1d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-04-28 11:37:10 +0200

    Add code

---
+++ graphene-fastapi-sqlalchemy/udemy-courses/building-graphql-apis-with-python-beginner-to-pro/README.md
@@ -0,0 +1,2 @@
+[Udemy course](https://www.udemy.com/course/building-graphql-apis-with-python/)
+The code has been retrieved from the course's resources.
---
+++ graphene-fastapi-sqlalchemy/udemy-courses/building-graphql-apis-with-python-beginner-to-pro/gql-app-main/main.py
@@ -0,0 +1,42 @@
+from graphene import Schema
+from fastapi import FastAPI
+from starlette_graphene3 import GraphQLApp, make_playground_handler
+from app.db.database import prepare_database, Session
+from app.db.models import Employer, Job, JobApplication
+from app.gql.queries import Query
+from app.gql.mutations import Mutation
+
+schema = Schema(query=Query, mutation=Mutation)
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def startup_event():
+    prepare_database()
+
+
+@app.get("/employers")
+def get_employers():
+    session = Session()
+    employers = session.query(Employer).all()
+    session.close()
+    return employers
+
+
+@app.get("/apps")
+def get_applications():
+    with Session() as session:
+        return session.query(JobApplication).count()
+
+
+@app.get("/jobs")
+def get_jobs():
+    with Session() as session:
+        return session.query(Job).all()
+
+
+app.mount("/", GraphQLApp(
+    schema=schema,
+    on_get=make_playground_handler()
+))
